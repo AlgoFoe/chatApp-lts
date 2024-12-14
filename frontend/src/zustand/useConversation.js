6f671be43705ffee2cd54d421989fd9b96e98bf8
Varun Singh <varun.singh10011@gmail.com>
2024-12-14 16:08:50 +0530

    Removed unnecessary comments from useConversation

--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -1,23 +1,3 @@
-// import { create } from "zustand";
-
-// const useConversation = create((set) => ({
-// 	selectedConversation: null,
-// 	setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
-// 	messages: [],
-// 	setMessages: (messages) => set({ messages }),
-// 	chatConversation: [],
-// 	setChatConversation: (chatConversation) => set({ chatConversation }),
-// 	formattedTimeOfLastMsg: '',
-//     setFormattedTimeOfLastMsg: (formattedTimeOfLastMsg) => set({ formattedTimeOfLastMsg }),
-//     lastMsgText: '',
-//     setLastMsgText: (lastMsgText) => set({ lastMsgText }),
-// 	receiverId: '',
-//     setReceiverId: (receiverId) => set({ receiverId }),
-// 	senderId: '',
-//     setSenderId: (senderId) => set({ senderId }),
-// }));
-
-// export default useConversation;
 import { create } from "zustand";
 
 const useConversation = create((set) => ({
